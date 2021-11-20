748e49428ff176c681adc2a9653aaca50be21582
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2021-11-20 18:57:55 +0100

    Update analysis scripts

---
+++ src/analysis/charging_stations_analysis.R
@@ -0,0 +1,73 @@
+# Charging Stations Data Analysis
+
+## Load data
+charging_stations <- data.table::fread("./data/charging_stations.csv",
+                                       header = TRUE,
+                                       sep = ",")
+
+## Set Types
+
+### Set charge device status to factor
+charging_stations$chargedevicestatus <- as.factor(charging_stations$chargedevicestatus)
+
+### Set connector count to factor
+charging_stations$connectorcount <- as.factor(charging_stations$connectorcount)
+
+## Create subset
+columns <- c("chargedeviceid",
+             "name",
+             "latitude",
+             "longitude",
+             "chargedevicestatus",
+             "datecreated",
+             "dateupdated",
+             "connectorcount")
+charging_stations_subset <- charging_stations[, ..columns]
+
+## Descriptive statistics
+
+### Summary
+summary(charging_stations_subset)
+
+stargazer::stargazer(charging_stations_subset,
+                     table.placement = "H",
+                     digit.separator = "",
+                     flip = TRUE,
+                     header = FALSE,
+                     label = "tab:charging_stations_numerical_summary_statistics",
+                     title = "Charging Stations (numerical) summary statistics")
+
+## Create plots
+
+### charging stations over time: timeseries
+ggplot2::ggplot(charging_stations_subset) +
+  ggplot2::aes(x = format(datecreated, "%Y")) +
+  ggplot2::geom_bar() +
+  ggplot2::theme_grey() +
+  ggplot2::labs(title = "Charging Stations Over Time",
+                x = "Dates",
+                y = "Frequency")
+
+# charge device status: barplot
+ggplot2::ggplot(charging_stations_subset) +
+  ggplot2::aes(x = chargedevicestatus) +
+  ggplot2::geom_bar() +
+  ggplot2::geom_text(stat = "count",
+                     ggplot2::aes(label = ggplot2::after_stat(count)),
+                     vjust = -1) +
+  ggplot2::theme_grey() +
+  ggplot2::labs(title = "Charge Device Status Distribution",
+                x = "Charge Device Status",
+                y = "Frequency")
+
+# connector count: barplot
+ggplot2::ggplot(charging_stations_subset) +
+  ggplot2::aes(x = connectorcount) +
+  ggplot2::geom_bar() +
+  ggplot2::geom_text(stat = "count",
+                     ggplot2::aes(label = ggplot2::after_stat(count)),
+                     vjust = -1) +
+  ggplot2::theme_grey() +
+  ggplot2::labs(title = "Connector Count Distribution",
+                x = "Connector Count",
+                y = "Frequency")
