1bc03f3a79f8927e6791dbe5d2ee1dbbbbb120ca
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2021-12-02 16:32:44 +0100

    Update heatmap script

--- src/heatmap/heatmap_ev.R
+++ src/heatmap/heatmap_ev.R
@@ -1,25 +1,22 @@
 library(dplyr)
 
+# Operators
+`%!in%` <- Negate(`%in%`)
+
 # Settings
 parking_zones_file <- "./data/parking_aggregated_ev.csv"
-charging_stations_file <- "./data/charging_stations_Scotland.csv"
+charging_stations_government_file <- "./data/charging_stations_government_scotland.csv"
+charging_stations_openchargemap_file <- "./data/charging_stations_openchargemap_scotland.csv"
 
 parking_zones_size_factor <- 1000
-colors_parking <- RColorBrewer::brewer.pal(9, "Greens")
-colors_parking <- colors_parking[6:9]
-parking_zones_color_map <- "#006D2C"
-parking_zones_color_bins <- 1
+parking_zones_color <- "#006D2C"
 parking_zones_opacity <- 0.5
 
 charging_stations_size_factor <- 50
-colors_charging <- RColorBrewer::brewer.pal(6, "Blues") #YlOrBr
-colors_charging <- colors_charging[3:6]
-charging_stations_color_map <- colors_charging
-charging_stations_color_bins <- 4
+charging_stations_color_map <- RColorBrewer::brewer.pal(8, "Blues")
 charging_stations_opacity <- 0.5
 
 map_latitude <- 57
-# 54.2086818
 map_longitude <- -2.751867
 map_zoom <- 7
 
@@ -40,28 +37,36 @@ server <- function(input, output) {
                                      sep = ",")
   parking_zones$longitude <- as.numeric(parking_zones$longitude)
   parking_zones$latitude <- as.numeric(parking_zones$latitude)
-
+  
   # Import charging stations data
-  charging_stations <- data.table::fread(charging_stations_file,
-                                         header = TRUE,
-                                         sep = ",")
-  charging_stations$longitude <- as.numeric(charging_stations$longitude)
-  charging_stations$latitude <- as.numeric(charging_stations$latitude)
+  charging_stations_government <- data.table::fread(charging_stations_government_file,
+                                                    header = TRUE,
+                                                    sep = ",")
+  charging_stations_government$longitude <- as.numeric(charging_stations_government$longitude)
+  charging_stations_government$latitude <- as.numeric(charging_stations_government$latitude)
+  charging_stations_government$source <- "GOVERNMENT"
+  
+  charging_stations_openchargemap <- data.table::fread(charging_stations_openchargemap_file,
+                                                       header = TRUE,
+                                                       sep = ",")
+  charging_stations_openchargemap$longitude <- as.numeric(charging_stations_openchargemap$longitude)
+  charging_stations_openchargemap$latitude <- as.numeric(charging_stations_openchargemap$latitude)
+  charging_stations_openchargemap$source <- "OPENCHARGEMAP"
+  
+  # Discard duplicate charging stations data
+  charging_stations_government <- charging_stations_government[charging_stations_government$latitude %!in% charging_stations_openchargemap$latitude &
+                                                               charging_stations_government$longitude %!in% charging_stations_openchargemap$longitude]
+  
+  # Merge charging stations data
+  charging_stations <- rbind(charging_stations_government, charging_stations_openchargemap)
 
   # Create parking zones size map
   parking_zones_size <- parking_zones$session_count / max(parking_zones$session_count) * parking_zones_size_factor
 
-  # Create parking zones color map
-  # parking_zones_color <- parking_zones[["session_average"]]
-  # parking_zones_palette <- leaflet::colorBin(parking_zones_color_map,
-  #                                            parking_zones_color,
-  #                                            parking_zones_color_bins,
-  #                                            pretty = TRUE)
-
   # Create charging stations color map
   charging_stations_color <- unique(charging_stations[["connectorcount"]])
   charging_stations_palette <- leaflet::colorFactor(charging_stations_color_map,
-                                                 charging_stations_color)
+                                                    charging_stations_color)
 
   # Create map
   output$map <- leaflet::renderLeaflet({
@@ -74,30 +79,50 @@ server <- function(input, output) {
                           radius = parking_zones_size,
                           stroke = FALSE,
                           fillOpacity = parking_zones_opacity,
-                          fillColor = "#006D2C") %>% #parking_zones_palette(parking_zones_color))
-
-      # # Add parking zones legend
-      # leaflet::addLegend("bottomleft",
-      #                    pal = parking_zones_palette,
-      #                    values = parking_zones_color,
-      #                    opacity = 1.0,
-      #                    title="Session Duration (Average)") %>%
-
+                          fillColor = parking_zones_color,
+                          popup = paste0("<b>",
+                                         "Zone ID: ",
+                                         htmltools::htmlEscape(parking_zones$zoneid),
+                                         "</b>",
+                                         "<br/>",
+                                         "Session Count: ",
+                                         parking_zones$session_count,
+                                         "<br/>",
+                                         "Session Average: ",
+                                         round(parking_zones$session_average, digits = 2),
+                                         " minutes")) %>%
+      
       # Add charging stations layer
       leaflet::addCircles(lng = charging_stations$longitude,
                           lat = charging_stations$latitude,
                           radius = charging_stations_size_factor,
                           stroke = FALSE,
                           fillOpacity = charging_stations_opacity,
-                          fillColor = charging_stations_palette(charging_stations_color)) %>%
-
+                          fillColor = charging_stations_palette(charging_stations_color),
+                          popup = paste0("<b>",
+                                         "Name: ",
+                                         htmltools::htmlEscape(charging_stations$name),
+                                         "</b>",
+                                         "<br/>",
+                                         "Location: ",
+                                         charging_stations$town,
+                                         "<br/>",
+                                         "Connector Status: ",
+                                         charging_stations$chargedevicestatus,
+                                         "<br/>",
+                                         "Connector Count: ",
+                                         charging_stations$connectorcount,
+                                         "<br/>",
+                                         "Source: ",
+                                         charging_stations$source)) %>%
+      
       # Add charging stations legend
       leaflet::addLegend("bottomright",
                          pal = charging_stations_palette,
                          values = charging_stations_color,
                          opacity = 1.0,
                          title="Connector Count") %>%
-
+      
       # Set base view
       leaflet::setView(lng = map_longitude, lat = map_latitude, zoom = map_zoom)
   })
